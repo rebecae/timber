f171dc90b5ba74f508acb2b057ff3b7e4a6e63fe
Rebeca <rebeca@Rebecas-MacBook-Pro.local>
2022-02-21 03:02:12 -0800

    complete

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <header className='header'> 
+      <span className='title'>Timber</span>
+      <div className='logo' />
+    </header>
+    </div>
+  );
+}
+
+export default App;
